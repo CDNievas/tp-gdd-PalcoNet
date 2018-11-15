4f2f63ec35f2d77ce22e04c5cafd4943f814ef0d
Daltobelli <denisealtobelli02@gmail.com>
2018-11-15 14:33:40 -0300

    login ahora usa caracter de unicode ● para la contra

--- app/PalcoNet/Form1.Designer.cs
+++ app/PalcoNet/Form1.Designer.cs
@@ -65,6 +65,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(58, 189);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '●';
             this.textBox2.Size = new System.Drawing.Size(147, 22);
             this.textBox2.TabIndex = 3;
             // 
