7191f76319a9b090101c44cd94398fd1357bbf58
Martin Mallea <mnmallea@gmail.com>
2018-11-22 17:24:11 -0300

    Fixing forms

--- app/PalcoNet/Program.cs
+++ app/PalcoNet/Program.cs
@@ -15,26 +15,11 @@ namespace PalcoNet
         /// </summary>
         [STAThread]
         static void Main()
-        {
-            /*
-            var rol = Roles.traerTodos().Find(r => r.nombre.Equals("ADMINISTRADOR"));
-
-            new Usuario("tincho", "ziracusa", rol).Persistite();
-            */
-            try
-            {
-                ValidadorLogin.ValidarLogin("tincho", "cualquier cosas");
-            }
-            catch (ProcedureException ex) {
-                Console.WriteLine(ex.InnerException.Message);
-            }
-            
-
-            /*
+        {   
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new logIn());
-            */
+            
         }
     }
 }
