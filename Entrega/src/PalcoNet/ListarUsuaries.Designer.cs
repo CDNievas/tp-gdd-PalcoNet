8b6d4760e42afc676c3bcc72fdc7742f4267dbcd
Martin Mallea <mmalleasantagada@est.frba.utn.edu.ar>
2018-12-20 12:48:40 -0300

    Funcionalidad para redar de alta usuarios

---
+++ Entrega/src/PalcoNet/ListarUsuaries.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PalcoNet
+{
+    partial class ListarUsuaries
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usuariesDataGrid = new System.Windows.Forms.DataGridView();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnHabilitar = new System.Windows.Forms.Button();
+            this.btnDeshabilitar = new System.Windows.Forms.Button();
+            this.btnPagAnt = new System.Windows.Forms.Button();
+            this.btnPagSig = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.usuariesDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usuariesDataGrid
+            // 
+            this.usuariesDataGrid.AllowUserToAddRows = false;
+            this.usuariesDataGrid.AllowUserToDeleteRows = false;
+            this.usuariesDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usuariesDataGrid.Location = new System.Drawing.Point(12, 46);
+            this.usuariesDataGrid.Name = "usuariesDataGrid";
+            this.usuariesDataGrid.ReadOnly = true;
+            this.usuariesDataGrid.Size = new System.Drawing.Size(670, 318);
+            this.usuariesDataGrid.TabIndex = 0;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(27, 20);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(101, 17);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(132, 20);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(267, 17);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(121, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnHabilitar
+            // 
+            this.btnHabilitar.Location = new System.Drawing.Point(352, 386);
+            this.btnHabilitar.Name = "btnHabilitar";
+            this.btnHabilitar.Size = new System.Drawing.Size(157, 23);
+            this.btnHabilitar.TabIndex = 4;
+            this.btnHabilitar.Text = "Eliminar/Dar de Alta";
+            this.btnHabilitar.UseVisualStyleBackColor = true;
+            this.btnHabilitar.Click += new System.EventHandler(this.btnHabilitar_Click);
+            // 
+            // btnDeshabilitar
+            // 
+            this.btnDeshabilitar.Location = new System.Drawing.Point(163, 386);
+            this.btnDeshabilitar.Name = "btnDeshabilitar";
+            this.btnDeshabilitar.Size = new System.Drawing.Size(169, 23);
+            this.btnDeshabilitar.TabIndex = 5;
+            this.btnDeshabilitar.Text = "Habilitar";
+            this.btnDeshabilitar.UseVisualStyleBackColor = true;
+            this.btnDeshabilitar.Click += new System.EventHandler(this.btnDeshabilitar_Click);
+            // 
+            // btnPagAnt
+            // 
+            this.btnPagAnt.Location = new System.Drawing.Point(12, 386);
+            this.btnPagAnt.Name = "btnPagAnt";
+            this.btnPagAnt.Size = new System.Drawing.Size(84, 23);
+            this.btnPagAnt.TabIndex = 6;
+            this.btnPagAnt.Text = "Pág Anterior";
+            this.btnPagAnt.UseVisualStyleBackColor = true;
+            this.btnPagAnt.Click += new System.EventHandler(this.btnPagAnt_Click);
+            // 
+            // btnPagSig
+            // 
+            this.btnPagSig.Location = new System.Drawing.Point(592, 386);
+            this.btnPagSig.Name = "btnPagSig";
+            this.btnPagSig.Size = new System.Drawing.Size(90, 23);
+            this.btnPagSig.TabIndex = 7;
+            this.btnPagSig.Text = "Pág. Siguiente";
+            this.btnPagSig.UseVisualStyleBackColor = true;
+            this.btnPagSig.Click += new System.EventHandler(this.btnPagSig_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(415, 17);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(121, 23);
+            this.btnLimpiar.TabIndex = 8;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // ListarUsuaries
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 447);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnPagSig);
+            this.Controls.Add(this.btnPagAnt);
+            this.Controls.Add(this.btnDeshabilitar);
+            this.Controls.Add(this.btnHabilitar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.usuariesDataGrid);
+            this.Name = "ListarUsuaries";
+            this.Text = "ListarUsuaries";
+            this.Load += new System.EventHandler(this.ListarUsuaries_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.usuariesDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView usuariesDataGrid;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnHabilitar;
+        private System.Windows.Forms.Button btnDeshabilitar;
+        private System.Windows.Forms.Button btnPagAnt;
+        private System.Windows.Forms.Button btnPagSig;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
---
+++ Entrega/src/PalcoNet/ListarUsuaries.cs
@@ -0,0 +1,131 @@
+﻿using PalcoNet.DataBasePackage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PalcoNet
+{
+    public partial class ListarUsuaries : Form
+    {
+        private Pagina pagina;
+        public ListarUsuaries()
+        {
+            InitializeComponent();
+            pagina = new Pagina(1, 15);
+        }
+
+        private void ListarUsuaries_Load(object sender, EventArgs e)
+        {
+            this.Text = "Usuarios";
+            ActualizarTabla();
+        }
+
+        private void ActualizarTabla()
+        {
+            usuariesDataGrid.DataSource = String.IsNullOrWhiteSpace(txtUsername.Text) ? BuscarUsuarios() : BuscarUsuariosPorNombre();
+        }
+
+        private DataTable BuscarUsuarios()
+        {
+            return DataBase.GetInstance().Query(@"select id_usuario as ID, username as [Nombre usuario],
+            intentos as [Cantidad de Intentos],
+            habilitado as [¿Habilitado?], eliminado as [¿Eliminado?]
+            from COMPUMUNDOHIPERMEGARED.Usuario "
+            + String.Format(" ORDER BY id_usuario OFFSET {0} ROWS FETCH NEXT {1} ROWS ONLY",
+            pagina.FirstResultIndex(), pagina.pageSize));
+        }
+
+        private DataTable BuscarUsuariosPorNombre()
+        {
+            return DataBase.GetInstance().TypedQuery(@"select id_usuario as ID, username as [Nombre usuario],
+            intentos as [Cantidad de Intentos],
+            habilitado as [¿Habilitado?], eliminado as [¿Eliminado?]
+            from COMPUMUNDOHIPERMEGARED.Usuario
+            where username like @namelike "
+                + String.Format(" ORDER BY id_usuario OFFSET {0} ROWS FETCH NEXT {1} ROWS ONLY",
+            pagina.FirstResultIndex(), pagina.pageSize)
+                , new QueryParameter("namelike", SqlDbType.NVarChar, "%" + txtUsername.Text + "%"));
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            this.pagina.pageNumber = 1;
+            ActualizarTabla();
+        }
+
+        //Habilitar / deshabilitar
+        private void btnDeshabilitar_Click(object sender, EventArgs e)
+        {
+            Usuarie usuarie;
+            try{
+                usuarie = SelectedUser();
+            }catch(Exception ex){
+                Console.WriteLine(ex + "\n" + ex.Message + "\n" + ex.StackTrace);
+                MessageBox.Show("Debe seleccionar un usuario");
+                return;
+            }
+            if (usuarie.Habilitado)
+            {
+                MessageBox.Show("Este usuario ya se encontraba habilitado");
+                return;
+            }
+            usuarie.Habilitar();
+            ActualizarTabla();
+        }
+
+        //Eliminar / dar de alta
+        private void btnHabilitar_Click(object sender, EventArgs e)
+        {
+            Usuarie usuarie;
+            try
+            {
+                usuarie = SelectedUser();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex + "\n" + ex.Message + "\n" + ex.StackTrace);
+                MessageBox.Show("Debe seleccionar un usuario");
+                return;
+            }
+            usuarie.CambiarEstadoEliminacion();
+            ActualizarTabla();
+        }
+
+        private Usuarie SelectedUser()
+        {
+            var cells = usuariesDataGrid.CurrentRow.Cells;
+            return new Usuarie(id:Convert.ToInt32(cells["ID"].Value),
+                habilitado: Convert.ToBoolean(cells["¿Habilitado?"].Value),
+                eliminado: Convert.ToBoolean(cells["¿Eliminado?"].Value));
+        }
+
+        private void btnPagAnt_Click(object sender, EventArgs e)
+        {
+            pagina.Previous();
+            ActualizarTabla();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            ActualizarTabla();
+        }
+
+        private void btnPagSig_Click(object sender, EventArgs e)
+        {
+            pagina.Next();
+            ActualizarTabla();
+            if (usuariesDataGrid.RowCount == 0)
+            {
+                pagina.Previous();
+                ActualizarTabla();
+            }
+        }
+    }
+}
